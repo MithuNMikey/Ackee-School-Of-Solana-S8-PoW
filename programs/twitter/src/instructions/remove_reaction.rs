1efd873d6181fcf934fde4c72dea6daa6ef8fcf0
Mithun R <123929717+MithuNMikey@users.noreply.github.com>
2025-11-06 00:56:50 +0530

    Implement remove reaction functionality
    Implemented the remove reaction functionality by decrementing the appropriate counter for likes or dislikes, and closing the tweet reaction account.

--- programs/twitter/src/instructions/remove_reaction.rs
+++ programs/twitter/src/instructions/remove_reaction.rs
@@ -1,28 +1,51 @@
-//-------------------------------------------------------------------------------
-///
-/// TASK: Implement the remove reaction functionality for the Twitter program
-/// 
-/// Requirements:
-/// - Verify that the tweet reaction exists and belongs to the reaction author
-/// - Decrement the appropriate counter (likes or dislikes) on the tweet
-/// - Close the tweet reaction account and return rent to reaction author
-/// 
-///-------------------------------------------------------------------------------
-
 use anchor_lang::prelude::*;
 
 use crate::errors::TwitterError;
 use crate::states::*;
 
 pub fn remove_reaction(ctx: Context<RemoveReactionContext>) -> Result<()> {
-    // TODO: Implement remove reaction functionality
-    todo!()
+    let tweet = &mut ctx.accounts.tweet;
+    let tweet_reaction = &ctx.accounts.tweet_reaction;
+    
+    // Decrement the appropriate counter based on reaction type
+    match tweet_reaction.reaction {
+        ReactionType::Like => {
+            require!(
+                tweet.likes > 0,
+                TwitterError::MinLikesReached
+            );
+            tweet.likes = tweet.likes.checked_sub(1).unwrap();
+        }
+        ReactionType::Dislike => {
+            require!(
+                tweet.dislikes > 0,
+                TwitterError::MinDislikesReached
+            );
+            tweet.dislikes = tweet.dislikes.checked_sub(1).unwrap();
+        }
+    }
+    
+    Ok(())
 }
 
 #[derive(Accounts)]
 pub struct RemoveReactionContext<'info> {
-    // TODO: Add required account constraints
+    #[account(mut)]
     pub reaction_author: Signer<'info>,
+    
+    #[account(
+        mut,
+        close = reaction_author,
+        seeds = [
+            TWEET_REACTION_SEED.as_bytes(),
+            reaction_author.key().as_ref(),
+            tweet.key().as_ref()
+        ],
+        bump = tweet_reaction.bump,
+        has_one = reaction_author
+    )]
     pub tweet_reaction: Account<'info, Reaction>,
+    
+    #[account(mut)]
     pub tweet: Account<'info, Tweet>,
 }
