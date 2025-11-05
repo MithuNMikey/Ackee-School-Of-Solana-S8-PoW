352883118511c12f6133d8d8aa800699661755a9
Mithun R <123929717+MithuNMikey@users.noreply.github.com>
2025-11-06 00:58:21 +0530

    Implement account constraints for remove_comment
    Added account constraints for removing comments.

--- programs/twitter/src/instructions/remove_comment.rs
+++ programs/twitter/src/instructions/remove_comment.rs
@@ -1,17 +1,4 @@
-//-------------------------------------------------------------------------------
-///
-/// TASK: Implement the remove comment functionality for the Twitter program
-/// 
-/// Requirements:
-/// - Close the comment account and return rent to comment author
-/// 
-/// NOTE: No implementation logic is needed in the function body - this 
-/// functionality is achieved entirely through account constraints!
-/// 
-///-------------------------------------------------------------------------------
-
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::hash::hash;
 
 use crate::states::*;
 
@@ -21,7 +8,13 @@ pub fn remove_comment(_ctx: Context<RemoveCommentContext>) -> Result<()> {
 
 #[derive(Accounts)]
 pub struct RemoveCommentContext<'info> {
-    // TODO: Add required account constraints
+    #[account(mut)]
     pub comment_author: Signer<'info>,
+    
+    #[account(
+        mut,
+        close = comment_author,
+        has_one = comment_author
+    )]
     pub comment: Account<'info, Comment>,
 }
